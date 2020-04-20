6bbaa08099c831902fccde042b428260f04ecbdb
yankasantos <yankasantos0107@gmail.com>
2020-04-20 17:26:25 -0300

    instalando a biblioteca require-dir para passar um comando de require com todos os arquivos que tiver na minha pasta models

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //Iniciando App
 const app = express();
 
 //Conectando ao Banco de Dados
 
-mongoose.connect('mongodb://localhost:27017/todoapi');
+mongoose.connect(
+  'mongodb://localhost:27017/todoapi'
+);
+requireDir('./api/models');
+
+
 
 //Criando rota
 app.get('/',(req, res) => {
