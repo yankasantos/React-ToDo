6d902b571497eb05bf11268fa599ff993ee544ec
yankasantos <yankasantos0107@gmail.com>
2020-04-20 17:58:45 -0300

    testando a conexão com o bnco, criando uma taskna base

--- back-end/server.js
+++ back-end/server.js
@@ -8,14 +8,24 @@ const app = express();
 //Conectando ao Banco de Dados
 
 mongoose.connect(
-  'mongodb://localhost:27017/todoapi'
+  'mongodb://localhost:27017/todoapi',
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  
+  }
 );
 requireDir('./api/models');
 
+const Task = mongoose.model('Tasks');
 
 
 //Criando rota
 app.get('/',(req, res) => {
+  Task.create({
+    title:'Criar Back-end da plicação To Do',
+  });
+
   res.send("Hello Wolrd NodeJS");
 });
 
