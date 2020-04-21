290ec525a91af74b4f1d54cd57ca5c5aa52ee66a
yankasantos <yankasantos0107@gmail.com>
2020-04-21 09:40:54 -0300

    instalando o cors e chamando ele na aplicação pra permitir requisição de outras url além de localhost, no futuro tem que configurar o dominio que pode acessar a api

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+
 
 //Iniciando App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Conectando ao Banco de Dados
 mongoose.connect(
